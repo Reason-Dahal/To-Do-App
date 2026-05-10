0ea7c62e9cd0a21702115698f3f5bdf79fe52786
Reason Dahal <rsondahal@gmail.com>
2026-05-10 08:22:34 +0545

    listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -12,25 +12,80 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.teal,
-          title: Center(child: Text("To Do List")),
-          leading: Icon(Icons.menu),
-          actions: [Icon(Icons.search)],
-        ),
-        body: Container(
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Text("To do App", style: TextStyle(fontSize: 30)),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> mylist = ["item 1", "item 2", "item 3"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarDemo(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      alignLabelWithHint: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 15),
+                ElevatedButton(onPressed: () {}, child: Text("add")),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: mylist.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    borderOnForeground: true,
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Icon(Icons.check_box),
+                      trailing: Icon(Icons.abc_outlined),
+                      title: Text(mylist[index]),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+class AppBarDemo extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "To Do App",
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.blueAccent,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
